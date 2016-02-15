ff187f1264a15cb036b18273dcdbd45c5338a743
Pawel2357 <2pawel.urbanski@gmail.com>
2016-02-15 15:22:54 +0100

    updated dane_skrypt.py

--- dane_skrypt.py
+++ dane_skrypt.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+import csv
+file_data = self.request.get('file_in')
+file_data_list = file_data.split('\n')
+file_Reader = csv.reader(file_data_list)
+for fields in file_Reader:
+    print row
